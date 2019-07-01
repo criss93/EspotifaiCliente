5622b278a9da867d5aa48788a347491ead4544ff
sebita22 <s_ferra3@hotmail.com>
2019-07-01 20:22:17 -0300

    Componentes degenerados

--- EspotifaiClient/src/app/app-routing.module.ts
+++ EspotifaiClient/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component'
+import {UserPageComponent} from './user-page/user-page.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'', component:LoginComponent},
+{path:'myPlaylists', component:UserPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- EspotifaiClient/src/app/app.module.ts
+++ EspotifaiClient/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
