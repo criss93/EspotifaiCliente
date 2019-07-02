8a693c883c1b432d466ee096cb1fd340a32bf1fb
criss <coccocristian@gmail.com>
2019-07-02 03:32:05 -0300

    avance

--- EspotifaiClient/src/app/app-routing.module.ts
+++ EspotifaiClient/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component'
+import {UserPageComponent} from './user-page/user-page.component'
+import { AuthGuard } from '../app/guards/auth.guard'
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path:'', component:LoginComponent},
+{path:'myPlaylists', component:UserPageComponent, canActivate: [AuthGuard]},
+{ path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- EspotifaiClient/src/app/app.module.ts
+++ EspotifaiClient/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserPageComponent } from './user-page/user-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
